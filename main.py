93a1a3d3d2f1bf16c86175100a06b3a6ab30ee05
dskolot <dskolot@gmail.com>
2026-05-24 23:29:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from core.config import BOT_TOKEN
 from bot.middlewares.auth import AuthMiddleware
 from bot.handlers import new_order, orders, finance
 from bot.handlers.start import router as start_router
+from bot.handlers import salary
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
@@ -24,6 +25,7 @@ async def main():
     dp.include_router(new_order.router)
     dp.include_router(orders.router)
     dp.include_router(finance.router)
+    dp.include_router(salary.router)
 
     logging.info("Бот запущен ✅")
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
